c8565a518e76b4f3aeafcb6a951c5e074fb220e2
Jun Jiang <jasl9187@hotmail.com>
2024-03-04 17:07:45 -0800

    wip

--- db/migrate/20240303103518_add_merkle_nodes_index.rb
+++ db/migrate/20240303103518_add_merkle_nodes_index.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
 class AddMerkleNodesIndex < ActiveRecord::Migration[7.2]
-  def up
-    add_index :merkle_nodes, :session
-    add_index :merkle_nodes, :begin_ts
-    add_index :merkle_nodes, :end_ts
-    add_index :merkle_nodes, :calculated_hash, where: "calculated_hash is null"
-  end
-
-  def down
-    remove_index :merkle_nodes, :session
-    remove_index :merkle_nodes, :begin_ts
-    remove_index :merkle_nodes, :end_ts
-    remove_index :merkle_nodes, :calculated_hash, where: "calculated_hash is null"
+  def change
+    change_table :merkle_nodes do |t|
+      t.index :session
+      t.index :begin_ts
+      t.index :end_ts
+      t.index :calculated_hash, where: "calculated_hash is null"
+    end
   end
 end
